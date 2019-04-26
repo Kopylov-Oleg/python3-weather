6cb618f675b8e3bdf96f857c0b327ca00dd96a95
Kopylov-Oleg <kopylov.oleg.7@mail.ru>
2019-04-26 15:34:21 +0300

    Added draw_weather() in Paint

--- weather.py
+++ weather.py
@@ -27,15 +27,18 @@ class WeatherApp(Frame):
         for i in range(12):
             self.rowconfigure(i, weight=1, minsize = 30)
 
-    def show_weather(self):
-        messagebox.showinfo("Вы ввели", get_weather_data(self.new_city.get()))
+    def show_weather(self):        
         self.Canvas.city.set(self.new_city.get())
+        self.Canvas.draw_weather()
 
 class Paint(Canvas):
     def __init__(self, master=None, *ap, default_city, **an):
         self.city = StringVar()
         self.city.set(default_city)
         Canvas.__init__(self, master, *ap, **an)
+
+    def draw_weather(self):
+        messagebox.showinfo("Вы ввели", get_weather_data(self.city))
 
 app = WeatherApp()
 app.mainloop()
