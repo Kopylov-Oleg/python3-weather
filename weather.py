09ec3973127f993cfba58401c04931f46585b51b
Kopylov-Oleg <kopylov.oleg.7@mail.ru>
2019-04-28 12:27:18 +0300

    Moved configure() to Paint()

--- weather.py
+++ weather.py
@@ -12,11 +12,10 @@ class WeatherApp(Frame):
         self.grid(sticky=N+E+S+W)
         self.new_city = StringVar()
         self.create()
-        self.adjust()
-        self.bind('<Configure>', self.configure)
+        self.adjust()        
 
     def create(self):
-        self.Canvas = Paint(self, default_city="moscow")
+        self.Canvas = Paint(self, default_city="")
         self.Canvas.grid(row=0, column=0, rowspan=9, columnspan=5, sticky=N+E+S+W)
         self.CityEntry = Entry(self, textvariable=self.new_city)
         self.CityEntry.grid(row=10, column=1, sticky=N+E+S+W)
@@ -33,18 +32,21 @@ class WeatherApp(Frame):
         self.Canvas.city.set(self.new_city.get())
         self.Canvas.draw_weather()
 
-    def configure(self, event):
-        if (self.new_city.get() != ""):
-            self.Canvas.draw_weather()
-
 class Paint(Canvas):
     def __init__(self, master=None, *ap, default_city,**an):
         self.city = StringVar()
         self.city.set(default_city)
         Canvas.__init__(self, master, *ap, **an)
+        self.bind('<Configure>', self.configure)
+    
+    def configure(self, event):
+        if (self.city.get() != ""):
+            messagebox.showinfo("Вы ввели", get_weather_data(self.city.get()))
+            #self.Canvas.draw_weather()
 
     def draw_weather(self):
         self.delete("all")
+        #messagebox.showinfo("Вы ввели", get_weather_data(self.city.get()))
         gui(self, get_weather_data(self.city.get()))
 
     def draw_city(self):
